9361476b358a1f8dcbe6d48661bf70d8940ee9c0
6th Hour <jh130349@student.wpsok.org>
2026-08-27 14:06:56 -0500

    Hjeohllo Worolld

--- HW3.py
+++ HW3.py
@@ -1,26 +1,27 @@
 #Name: Jerrell Amari Huffman
 #Class: 6th Hour
 #Assignment: HW3
-from token import tok_name
+#from token import tok_name
 
 #1. Print "Hello World!"
 print("Hello World")
 #2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
-Bobby = 12
-Tina = False
-Terry = "pluh"
+O = 1
+L = True
+B = ("Hello")
 #3. Print all three variables on the same print function (at the same time).
-print(Bobby, Tina, Terry)
+print(O, L , B)
 #4. Create a variable that asks the user to input an integer.
-input("input a silly integer")
+EGL = int(input("IPUT INTEGER "))
 #5. Add the integer variable from #2 with the integer from #4 and print the result.
-print(Bobby, Tina, Terry)
+JHK = int(input(O + EGL))
 #6. Take the result from #5 and divide it by 2. Print the result.
-
+print(JHK / 2)
 #7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
-
+L = False
 #8. Print the value of the boolean variable.
-
+print(L)
 #9. Create a variable with a number that contains decimals.
-
+V = 4.45106673956294556274925
 #10. Round the number from #9 up or down using the round function.
+round(V)
