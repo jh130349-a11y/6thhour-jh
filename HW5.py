c0677d40effd487907633a89a10d969d4695261e
6th Hour <jh130349@student.wpsok.org>
2026-09-10 13:29:38 -0500

    HW5 lol.

---
+++ HW5.py
@@ -0,0 +1,24 @@
+#Name: Jerrell Amari Huffman
+#Class: 6th Hour
+#Assignment: HW5
+
+#1. Print Hello World!
+print("hello world")
+#2. Create a list with 5 strings containing 5 different names in it.
+list1 = ["Bebog", "Bobby", "Bibo", "Blare", "Barista"]
+#3. Print out the 4th name on the list.
+print(list1[3])
+#4. Create a list with 4 different integers in it.
+list2 = [13, 23, 32, 49]
+#5. Insert a new integer into the 2nd spot and print the new list.
+list2.insert(1,67)
+print(list2)
+#6. Sort the list from lowest to highest and print the sorted list.
+list2.sort()
+print(list2)
+#7. Add the 1st three numbers on the sorted list together and print the sum.
+print(13 + 23 + 32)
+#8. Create a list with two strings, two variables, and too boolean values.
+list3 = ["str1", "str2", True, False, 85, 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000]
+#9. Create a print statement that asks the user to input their own index value for the list on #8.
+print(int(input("input your own index value for the list ")))
