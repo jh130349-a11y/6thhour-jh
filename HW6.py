823a4e12e4836d844fe49608e623c5b03a3ebcd4
6th Hour <jh130349@student.wpsok.org>
2026-09-10 13:45:31 -0500

    HW6 lol.

---
+++ HW6.py
@@ -0,0 +1,29 @@
+#Name: Jerrell Amari Huffman
+#Class: 6th Hour
+#Assignment: HW6
+
+
+#1. Create a list with 9 different numbers inside.
+the_list = [6, 7, 8, 9, 10, 11, 12, 13, 14]
+print(the_list)
+#2. Sort the list from highest to lowest.
+the_list.sort(reverse=True)
+print(the_list)
+#3. Create an empty list.
+the_empty_list = []
+print(the_empty_list)
+#4. Remove the median number from the first list and add it to the second list.
+the_list.remove(8)
+print(the_list)
+#5. Remove the first number from the first list and add it to the second list.
+the_empty_list.append(8)
+#6. Print both lists.
+print(the_empty_list)
+print(the_list)
+#7. Add the two numbers in the second list together and print the result.
+pluhh = ((6 in the_empty_list) + (7 in the_empty_list))
+#8. Add the sum from #7 to the first list.
+the_list.append(pluhh)
+#9. Sort the first list from lowest to highest and print it.
+the_list.sort(reverse=True)
+print(the_list)
